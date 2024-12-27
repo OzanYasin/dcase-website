7c2979c4b2df46b9b5c9ac45f5302c14d4a12a7c
Ozan <ozan.yasin97@gmail.com>
2024-12-27 10:06:15 +0300

    add missed business value cards

--- components/landing/business-value-section/benefits.tsx
+++ components/landing/business-value-section/benefits.tsx
@@ -1,4 +1,4 @@
-import { DollarSign, ShieldCheck, Zap } from "lucide-react";
+import { DollarSign, ShieldCheck, ShieldCheckIcon, Zap } from "lucide-react";
 import { BenefitCard } from "./benefit-card";
 
 const benefits = [
@@ -19,6 +19,24 @@ const benefits = [
     title: "Faster ROI & Lower TCO",
     description:
       '<br/><span class="font-bold">Slash Operational Costs:</span> DCase\'s intelligent automation reduces manual tasks by up to 40%, leading to substantial cost savings.<br/><br/><span class="font-bold">Simplified Licensing</span>: Flexible pricing models and modular architecture ensure you only pay for what you truly need.',
+  },
+  {
+    icon: Zap,
+    title: "Accelerated Digital Transformation",
+    description:
+      '<br/><span class="font-bold">Agile Customization:</span> Our Template Manager and Workflow Engine adapt to your ever-evolving processes, ensuring a seamless transformation journey.<br/><br/><span class="font-bold">Unify Disparate Systems:</span> Bring all your IT assets under one roof. Streamlined workflows, integrated data, better decision-making.',
+  },
+  {
+    icon: ShieldCheckIcon,
+    title: "Enhanced Customer Satisfaction",
+    description:
+      '<br/><span class="font-bold">Faster Ticket Resolutions:</span> Leverage data-driven insights and intelligent workflows to ensure quick and effortless issue resolution.<br/><br/><span class="font-bold">Boost User Engagement:</span> A clear, flexible platform reduces frustration, fosters collaboration, and improves overall service culture.',
+  },
+  {
+    icon: DollarSign,
+    title: "Scalable Growth",
+    description:
+      '<br/><span class="font-bold">Future-Proof IT Operations:</span> DCase evolves with your business, enabling fast adaptation to new challenges and industry demands without disruption.<br/><br/><span class="font-bold">Flexible and Cost-Effective Scaling:</span> Scale effortlessly as your needs grow, paying only for what you use while maintaining full control over costs.',
   },
 ];
 
